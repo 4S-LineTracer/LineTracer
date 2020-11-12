6da3edf3ce14e885f3335a052f3dd7f5abbed961
Enchan <enchant0130@gmail.com>
2020-11-12 23:18:24 +0900

    [Update] ITU Utility define

--- Soft2/initialize.c
+++ Soft2/initialize.c
@@ -8,23 +8,23 @@
 //  - Returns: なし
 void initialize(){
     // ITU0は0.1ms間隔のパルスを出力、ITU1はそれをカウンタとする
-    TCR0 = 0xA0;
+    TCR0 = ITU_CLR_GRA | ITU_EDGE_RISE | ITU_PS_1;
     TIOR0 = 0x8B;
     GRA0 = 625 - 1;
 
-    TCR1 = 0xA6;
+    TCR1 = ITU_CLR_GRA | ITU_EDGE_RISE | ITU_PS_EXT_C;
     TIER1 = 0x01;
     GRA1 = 999 - 1; // 初期値(ダミー)
 
     // ITU2: 1ms/interrupt
-    TCR2 = 0xA6; // 1010 0001
+    TCR2 = ITU_CLR_GRA | ITU_EDGE_RISE | ITU_PS_8;
     TIER2 = 0x01;
-    GRA2 = 100 - 1;
+    GRA2 = 3125 - 1; // 40 * 8 = 320ns 0.32us. 1ms / 320n = 3,125.
 
 	// ITU3: 10ms/interrupt
-    TCR3 = 0xA6; // 1010 0100
+    TCR3 = ITU_CLR_GRA | ITU_EDGE_RISE | ITU_PS_8;
     TIER3 = 0x01;
-    GRA3 = 10 - 1;
+    GRA3 = 31250 - 1; // 40 * 8 = 320ns 0.32us. 10ms / 320n = 31,250.
 
     // 各種共有変数の値を初期化
     MOTOR_SPEED = 0x00;
@@ -40,3 +40,4 @@ void initialize(){
     // 割込み有効化
 	and_ccr(0x7F);
 }
+
--- Soft2/initialize.h
+++ Soft2/initialize.h
@@ -9,4 +9,24 @@
 /* -------- */
 void initialize();
 
+/* -------- */
+#define ITU_PS_1 0x00
+#define ITU_PS_2 0x01
+#define ITU_PS_4 0x02
+#define ITU_PS_8 0x03
+
+#define ITU_PS_EXT_A 0x04
+#define ITU_PS_EXT_B 0x05
+#define ITU_PS_EXT_C 0x06
+#define ITU_PS_EXT_D 0x07
+
+#define ITU_EDGE_RISE (0x00 << 3)
+#define ITU_EDGE_FALL (0x01 << 3)
+#define ITU_EDGE_ALL (0x02 << 3)
+
+#define ITU_CLR_NEVER (0x00 << 5)
+#define ITU_CLR_GRA (0x01 << 5)
+#define ITU_CLR_GRB (0x02 << 5)
+#define ITU_CLR_SYNC (0x03 << 5)
+
 #endif
